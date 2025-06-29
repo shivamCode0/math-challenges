fda4f54acb9252b1828d67050f99c5d5cce99349
Shivam <47765144+shivamCode0@users.noreply.github.com>
2025-06-29 11:12:46 +0000

    fix [skip-ci]

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,13 +3,15 @@ import Search from "./Search";
 import { useLoading } from "../contexts/LoadingContext";
 import { usePrintMode } from "../contexts/PrintModeContext";
 import { useSearchQuery } from "../contexts/SearchQueryContext";
+import { useDarkMode } from "contexts/useDarkMode";
 
 function Layout({ children }) {
   const { printMode } = usePrintMode();
   const { searchQuery, setSearchQuery } = useSearchQuery();
+  const dm = useDarkMode();
 
   return (
-    <div className={`app${printMode ? " no-print" : ""}`}>
+    <div className={`app${printMode ? " no-print" : ""}${dm.darkModeEnabled ? " dark-on" : ""}`}>
       <Nav {...{ searchQuery, setSearchQuery }} />
       {!!searchQuery && <Search search={searchQuery} setSearch={setSearchQuery} />}
       <div {...(searchQuery && { style: { display: "none" } })}>{children}</div>
